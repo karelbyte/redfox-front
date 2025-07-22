c10a15556008b27a52dd03144c0a3c7479ef0e02
Karel Puerto Diaz <karelpuerto78@gmail.com>
2025-07-22 12:57:29 -0600

    burd url back

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const baseURL = (process.env.NEXT_PUBLIC_URL_API || 'https://nitro-api-d80s.onrender.com') + '/api';
+const baseURL = (process.env.NEXT_PUBLIC_URL_API || 'https://nitro-api-app-production.up.railway.app') + '/api';
 //const baseURL = 'https://nitro-api-app-production-4b41.up.railway.app/api';
 const handleUnauthorized = () => {
   if (typeof window !== 'undefined') {
